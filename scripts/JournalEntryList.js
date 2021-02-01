83ac8556db93b0d6815dca7e2c08363f3456f868
Dan Ross <daniellross@gmail.com>
2021-02-01 15:41:26 -0600

    Added api, copied in data from old dataprovider object, added fetch api call, and updated funcitons so posts from api render in dom on pageload

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,19 +1,21 @@
-import { UseJournalEntries } from './JournalDataProvider.js'
+import { UseJournalEntries, getEntries } from './JournalDataProvider.js'
 import { JournalEntryComponent } from './JournalEntry.js'
 
 
 export const EntryListComponent = () => {
-    const entries = UseJournalEntries()
-    const entryLog = document.querySelector('.container')
+    getEntries()
+        .then(() => {
+            const entries = UseJournalEntries()
+            const entryLog = document.querySelector('.container')
+            let entryHTML = ""
+            for (const entry of entries) {
+                entryHTML += JournalEntryComponent(entry)
+            }
 
-    let entryHTML = ""
-    for (const entry of entries) {
-        entryHTML += JournalEntryComponent(entry)
-    }
-
-    entryLog.innerHTML += `
+            entryLog.innerHTML += `
         <section class="journalContainer">
             ${entryHTML}
         </section>
     `
+        })
 }
