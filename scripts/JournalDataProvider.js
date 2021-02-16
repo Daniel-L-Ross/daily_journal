03702b41a47483f4850b5934ee2e0b004ee6c083
Dan Ross <daniellross@gmail.com>
2021-02-16 11:42:24 -0600

    moods now populate a list on the form and the mood pk is saved as a fk

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,4 +1,5 @@
 let journal = []
+let moods = []
 
 const eventHub = document.querySelector(".container")
 
@@ -11,6 +12,11 @@ export const UseJournalEntries = () => {
     return sortedByDate
 }
 
+export const useMoods = () => {
+    return moods.slice()
+
+}
+
 const dispatchStateChangeEvent = () => {
     const entriesChangedEvent = new CustomEvent("entryStateChanged")
     eventHub.dispatchEvent(entriesChangedEvent)
@@ -18,15 +24,15 @@ const dispatchStateChangeEvent = () => {
 
 export const getEntries = () => {
     return fetch("http://localhost:8088/entries")
-    .then(response => response.json())
-    .then(
-        parsedEntries => {
-            journal = parsedEntries
-        }
+        .then(response => response.json())
+        .then(
+            parsedEntries => {
+                journal = parsedEntries
+            }
         )
-    }
-    
-    export const saveEntry = (entryObject) => {
+}
+
+export const saveEntry = (entryObject) => {
     return fetch("http://localhost:8088/entries", {
         method: "POST",
         headers: {
@@ -36,4 +42,14 @@ export const getEntries = () => {
     })
     .then(getEntries)
     .then(dispatchStateChangeEvent)
+}
+
+export const getMoods = () => {
+    return fetch("http://localhost:8088/moods")
+        .then(response => response.json())
+        .then(
+            parsedMoods => {
+                moods = parsedMoods
+            }
+        )
 }
--- scripts/JournalEntryForm.js
+++ scripts/JournalEntryForm.js
@@ -1,44 +1,43 @@
-import { saveEntry } from "./JournalDataProvider.js"
+import { getMoods, saveEntry, useMoods } from "./JournalDataProvider.js"
 
 const eventHub = document.querySelector(".container")
 
 const contentElement = document.querySelector(".formWrapper")
 
 export const JournalEntryForm = () => {
-    contentElement.innerHTML = `
-    <section class="formContainer">
-    <form action="">
+
+    getMoods()
+        .then(() => {
+            const moods = useMoods()
+
+            contentElement.innerHTML = `
+        <section class="formContainer">
+        <form action="">
         <fieldset>
-            <label for="journalDate">Date of Entry</label>
-            <input type="date" name="journalDate" id="journalDate" class="formOption">
+        <label for="journalDate">Date of Entry</label>
+        <input type="date" name="journalDate" id="journalDate" class="formOption">
         </fieldset>
         <fieldset>
-            <label for="conceptsCovered">Concepts Covered</label>
-            <input type="text" name="conceptsCovered" id="conceptsCovered" class="formOption">
+        <label for="conceptsCovered">Concepts Covered</label>
+        <input type="text" name="conceptsCovered" id="conceptsCovered" class="formOption">
         </fieldset>
         <fieldset>
-            <label for="journalEntry">Journal Entry</label>
-            <textarea type="textarea" name="journalEntry" id="journalEntry" class="formOption"></textarea>
+        <label for="journalEntry">Journal Entry</label>
+        <textarea type="textarea" name="journalEntry" id="journalEntry" class="formOption"></textarea>
         </fieldset>
         <fieldset>
-            <label for="mood">Mood for the Day</label>
-            <select name="mood" id="mood">
-                <option value="0">Please select a mood...</option>
-                <option value="happy">Happy</option>
-                <option value="excited">Excited</option>
-                <option value="calm">Calm</option>
-                <option value="content">Content</option>
-                <option value="relaxed">Relaxed</option>
-                <option value="sad">Sad</option>
-                <option value="nervous">Nervous</option>
-                <option value="upset">Upset</option>
-                <option value="tense">Tense</option>
-            </select>
+        <label for="mood">Mood for the Day</label>
+        <select name="mood" id="mood">
+        <option value="0">Please select a mood...</option>
+            ${moods.map(mood =>`<option value="${mood.id}">${mood.label}</option>`).join("")}
+        </select>
         </fieldset>
         <button class="button postEntry__button" id="postEntry">Post Entry</button>
-    </form>
-</section>
-    `
+        </form>
+        </section>
+        `
+        })
+
 }
 
 eventHub.addEventListener("click", clickEvent => {
@@ -48,7 +47,7 @@ eventHub.addEventListener("click", clickEvent => {
             date: document.querySelector('#journalDate').value,
             concept: document.querySelector('#conceptsCovered').value,
             entry: document.querySelector('#journalEntry').value,
-            mood: document.querySelector('#mood').value
+            moodId: parseInt(document.querySelector('#mood').value)
         }
         debugger
         if (newEntry.date === "") {
@@ -57,10 +56,10 @@ eventHub.addEventListener("click", clickEvent => {
             alert("You must enter a concept before saving. Please limit the description to 25 characters or less.")
         } else if (newEntry.entry === "" || newEntry.entry.length > 200) {
             alert("You must add an entry before saving. Please limit the entry to 200 characters or less.")
-        } else if (parseInt(newEntry.mood) === 0) {
+        } else if (newEntry.moodId === 0 ){
             alert("Please select a mood before saving an entry.")
         }
-        if (newEntry.date !== "" && newEntry.concept.length > 0 && newEntry.concept.length < 25 && newEntry.entry.length > 0 && newEntry.entry.length < 200 && parseInt(newEntry.mood) !== 0) {
+        if (newEntry.date !== "" && newEntry.concept.length > 0 && newEntry.concept.length < 25 && newEntry.entry.length > 0 && newEntry.entry.length < 200 && newEntry.moodId !== 0) {
 
             saveEntry(newEntry)
         }
